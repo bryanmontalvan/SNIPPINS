9861fdbad238eadf9b7ed222411b76fe1ae5aa51
Bryan <bmontalvan100@gmail.com>
2021-10-27 17:59:11 -0400

    First iteration: 1. Using MediaRecorder interface to record users screen 2. Storing video produced from MediaRecorder and storing in a video tag 3. Implemented playing the video and downloading using a button.
    Features to implement:
    1. Rewrite using Electron for Desktop usage
    2. Upgrade UI when video is ready to be downloaded

---
+++ index.js
@@ -0,0 +1,54 @@
+let btn = document.querySelector(".record-btn")
+let btn_save = document.querySelector(".download-video")
+
+btn.addEventListener("click", async function () {
+	let stream = await navigator.mediaDevices.getDisplayMedia({
+		video: true
+	})
+
+	//needed for better browser support
+	const mime = MediaRecorder.isTypeSupported("video/webm; codecs=vp9")
+		? "video/webm; codecs=vp9"
+		: "video/webm"
+	let mediaRecorder = new MediaRecorder(stream, {
+		mimeType: mime
+	})
+
+	let chunks = []
+	mediaRecorder.addEventListener('dataavailable', function (e) {
+		chunks.push(e.data)
+	})
+
+	mediaRecorder.addEventListener('stop', function () {
+		let blob = new Blob(chunks, {
+			type: chunks[0].type
+		})
+		let url = URL.createObjectURL(blob)
+
+		let video = document.querySelector("video")
+		video.src = url
+
+		btn_save.style.background = "#3b6df7";
+	})
+
+	btn_save.addEventListener("click", function () {
+		let blob = new Blob(chunks, {
+			type: chunks[0].type
+		})
+		let url = URL.createObjectURL(blob)
+
+		let video = document.querySelector("video")
+		video.src = url
+
+		let a = document.createElement('a')
+		a.href = url
+		a.download = 'video.webm'
+		a.click()
+	})
+
+
+
+	//we have to start the recorder manually
+	mediaRecorder.start()
+})
+
