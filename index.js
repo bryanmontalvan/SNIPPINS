6515a29d54df1237f4f792a3b8e467785f51e2a9
Bryan <bmontalvan100@gmail.com>
2021-10-27 20:20:47 -0400

    Commented JS, Fixed github link, and some styling

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ btn.addEventListener("click", async function () {
 		video: true
 	})
 
-	//needed for better browser support
 	const mime = MediaRecorder.isTypeSupported("video/webm; codecs=vp9")
 		? "video/webm; codecs=vp9"
 		: "video/webm"
@@ -14,23 +13,28 @@ btn.addEventListener("click", async function () {
 		mimeType: mime
 	})
 
+	// Runs when MediaRecorder delivers media data 
+	// Chunks gives us "chunks" of the data while the video is being recorded
 	let chunks = []
-	mediaRecorder.addEventListener('dataavailable', function (e) {
+	mediaRecorder.addEventListener('dataavailable', function (e) { 
 		chunks.push(e.data)
 	})
 
 	mediaRecorder.addEventListener('stop', function () {
+		// Create blob which contains 
 		let blob = new Blob(chunks, {
 			type: chunks[0].type
 		})
 		let url = URL.createObjectURL(blob)
 
+		// Display video using DOM
 		let video = document.querySelector("video")
 		video.src = url
-
+		// Change backgrounnd from button sav
 		btn_save.style.background = "#3b6df7";
 	})
 
+	// On click reinitialize blob and download the video
 	btn_save.addEventListener("click", function () {
 		let blob = new Blob(chunks, {
 			type: chunks[0].type
@@ -48,7 +52,7 @@ btn.addEventListener("click", async function () {
 
 
 
-	//we have to start the recorder manually
+	// We have to start the recorder manually
 	mediaRecorder.start()
 })
 
